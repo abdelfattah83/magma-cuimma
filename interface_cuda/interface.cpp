6fa702c057203e4fed759b263c6bb7bb455ab1e5
Ahmad Abdelfattah <ahmad@icl.utk.edu>
2024-10-24 14:38:45 -0400

    minor

--- interface_cuda/interface.cpp
+++ interface_cuda/interface.cpp
@@ -1097,6 +1097,7 @@ magma_queue_create_internal(
 #endif
 
     queue->cuimma_nsplits__ = 18; // most accurate version
+    queue->cuimma_work__    = NULL;
 
     // custom workspace for cuIMMA
     #ifdef MAGMA_HAVE_CUDA
@@ -1222,7 +1223,9 @@ magma_queue_create_from_cuda_internal(
     stat2 = cusparseSetStream( queue->cusparse__, queue->stream__ );
     check_xerror( stat2, func, file, line );
 
-	queue->cuimma_nsplits__ = 18; // most accurate version
+    queue->cuimma_nsplits__ = 18; // most accurate version
+    queue->cuimma_work__    = NULL;
+
     // custom workspace for cuIMMA
     #ifdef MAGMA_HAVE_CUDA
     magma_int_t magma_arch = magma_getdevice_arch();
